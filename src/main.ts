3cd58cc902c674b99b88ddba9bf632ec119303b0
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-19 20:33:01 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,20 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+    // Use CORS
+    app.enableCors();
+
     // Use Logger
     const logger: Logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
     app.useLogger(logger);
 
     // Use Config
     const configService: ConfigService = app.get(ConfigService);
-    const PORT = configService.get<number>('PORT');
-    const HOST = configService.get<string>('HOST');
+    const PORT = configService.get<number>('PORT') || 3000;
+    const HOST = configService.get<string>('HOST') || 'localhost';
 
     await app.listen(PORT || 3000, () => {
-        console.log(`Application start at : http://${HOST}:${PORT || 3000}`);
+        console.log(`Application start at : http://${HOST}:${PORT}`);
     });
 }
 bootstrap();
