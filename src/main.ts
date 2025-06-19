728b4dd8596de8b6a3b0150c0837fadc4e1959cb
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-19 22:14:24 +0700

    add openapi

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ConfigService } from '@nestjs/config';
 import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 import { ErrorFilter } from './common/filter/error.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -48,6 +49,16 @@ async function bootstrap() {
     const configService: ConfigService = app.get(ConfigService);
     const PORT = configService.get<number>('PORT') || 3000;
     const HOST = configService.get<string>('HOST') || 'localhost';
+
+    // OpenApi Swagger
+    const docsConfig = new DocumentBuilder()
+        .setTitle('Template')
+        .setDescription('Template Description')
+        .addBearerAuth()
+        .build();
+
+    const docs = SwaggerModule.createDocument(app, docsConfig);
+    SwaggerModule.setup('api', app, docs);
 
     await app.listen(PORT, () => {
         console.log(`Application start at : http://${HOST}:${PORT}`);
