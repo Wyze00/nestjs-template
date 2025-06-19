62efd680b17449bfc7ee385aeca12b1e8775cd2a
Wyze00 <nathantanusubroto15@gmail.com>
2025-06-19 20:45:04 +0700

    enable validator

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,27 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    // Use CORS
+    // Enable CORS
     app.enableCors();
+
+    // Enable Validator
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+        }),
+    );
 
     // Use Logger
     const logger: Logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
     app.useLogger(logger);
 
-    // Use Config
+    // Configuration
     const configService: ConfigService = app.get(ConfigService);
     const PORT = configService.get<number>('PORT') || 3000;
     const HOST = configService.get<string>('HOST') || 'localhost';
