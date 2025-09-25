7b0ec43fe6896fc175f658921af921b04d6b4451
Wyze00 <nathantanusubroto15@gmail.com>
2025-09-25 16:53:16 +0700

    update Winston, save all logs to a file

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -11,8 +11,25 @@ import * as Joi from 'joi';
 @Module({
     imports: [
         WinstonModule.forRoot({
-            format: winston.format.json(),
-            transports: [new winston.transports.Console()],
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.simple(),
+                    ),
+                }),
+                new winston.transports.File({
+                    filename: 'logs/error.log',
+                    level: 'error',
+                }),
+                new winston.transports.File({
+                    filename: 'logs/combined.log',
+                }),
+            ],
             level: 'debug',
         }),
         ConfigModule.forRoot({
